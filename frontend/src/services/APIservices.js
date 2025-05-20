18b103fa56f71ee2ec8fae7acc54e8cd5f72f4e1
levski1914 <georgi.dimitrov@abv.bg>
2025-05-20 18:32:35 +0300

    Update APIservices.js

--- frontend/src/services/APIservices.js
+++ frontend/src/services/APIservices.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://forecast-l4jx.onrender.com";
+export const BACKEND_URL = "https://forecast-production-81c9.up.railway.app";
 // export const BACKEND_URL = "http://localhost:5000";
